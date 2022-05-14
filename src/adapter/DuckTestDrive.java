3bc5aacd7348ab3bb4de26c0b76224a380035861
MOO <minjae.lee@altiall.com>
2022-05-14 15:33:07 +0900

    feat: 기본 Adapter pattern 코드 추가

---
+++ src/adapter/DuckTestDrive.java
@@ -0,0 +1,19 @@
+package adapter;
+
+public class DuckTestDrive {
+
+    public static void main(String[] args) {
+        Duck mallardDuck = new MallardDuck();
+
+        Turkey wildTurkey = new WildTurkey();
+        TurkeyAdapter turkeyAdapter = new TurkeyAdapter(wildTurkey);
+
+        testDuck(mallardDuck);
+        testDuck(turkeyAdapter);
+    }
+
+    static void testDuck(Duck duck){
+        duck.quack();
+        duck.fly();
+    }
+}
